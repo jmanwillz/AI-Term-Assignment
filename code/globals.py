9ff8c86c4b90155d1d0d1b4f84146fb3c81faef3
Jason Wille <jason.wille@advance.io>
2024-05-18 15:37:15 +0200

    Changes.

--- code/globals.py
+++
@@ -1,5 +0,0 @@
-class Globals:
-    number_iterations = 50
-    run_path = "domains"
-    train_file_name = "runstats.csv"
-    domain_name = "15-puzzle"
--- code/test_domain_data.py
+++
@@ -1,3 +0,0 @@
-class TestDomainData:
-    def __init__(self) -> None:
-        pass
